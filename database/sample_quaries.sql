412fad45cf1bae2d263c184ad0220780cffc0979
apekshacrao <apekshacrao@gmail.com>
2026-07-08 23:43:59 +0530

    Added sample SQL queries

---
+++ database/sample_quaries.sql
@@ -0,0 +1,9 @@
+-- Display all users
+SELECT * FROM users;
+
+-- Display all resumes
+SELECT * FROM resumes;
+
+-- Insert a sample user
+INSERT INTO users(name, email, password)
+VALUES ('Sample User', 'sample@example.com', 'password123');
